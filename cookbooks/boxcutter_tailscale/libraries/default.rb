61c345461f58f10e70b157b1e58b03127e104ff9
Mischa Taylor <taylor@linux.com>
2024-04-20 08:15:40 -0400

    Fix cookstyle issues

--- cookbooks/boxcutter_tailscale/libraries/default.rb
+++ cookbooks/boxcutter_tailscale/libraries/default.rb
@@ -6,16 +6,14 @@ module Boxcutter
       include Chef::Mixin::ShellOut
 
       def load_config
-        begin
-          result = shell_out!('/usr/bin/tailscale debug prefs')
-          JSON.parse(result.stdout)
-        rescue Mixlib::ShellOut::ShellCommandFailed => e
-          Chef::Log.error("boxcutter_tailscale: load_config() failed to execute command: #{e.message}")
-          return nil
-        rescue JSON::ParserError => e
-          Chef::Log.error("boxcutter_tailscale: load_config() JSON parsing failed: #{e.message}")
-          return nil
-        end
+        result = shell_out!('/usr/bin/tailscale debug prefs')
+        JSON.parse(result.stdout)
+      rescue Mixlib::ShellOut::ShellCommandFailed => e
+        Chef::Log.error("boxcutter_tailscale: load_config() failed to execute command: #{e.message}")
+        return nil
+      rescue JSON::ParserError => e
+        Chef::Log.error("boxcutter_tailscale: load_config() JSON parsing failed: #{e.message}")
+        return nil
       end
     end
   end
