1ad919e1fbdc2725c8f3abdcad3a751dd55520e8
Mischa Taylor <taylor@linux.com>
2024-06-15 18:12:17 -0400

    Make lintastic

--- cookbooks/boxcutter_users/recipes/default.rb
+++ cookbooks/boxcutter_users/recipes/default.rb
@@ -42,5 +42,7 @@ node.default['fb_sudo']['users']['%sudo']['dont prompt for password'] = 'ALL=NOP
 node.default['fb_ssh']['enable_central_authorized_keys'] = true
 # node.default['fb_ssh']['enable_central_authorized_principals'] = true
 
-node.default['fb_ssh']['authorized_keys']['taylor']['mahowald'] = 'ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDBZjVID1mAqZyhD3p0VbJtidKAxMHUwLmEMaCAJX0UN mahowald'
-node.default['fb_ssh']['authorized_keys']['sheila']['sheila'] = 'ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINy9cJcJl8oN6bRtcBc4RZq8f/T6P1AFR3YS1YRYi5YY sheila'
+node.default['fb_ssh']['authorized_keys']['taylor']['mahowald'] = 
+  'ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDBZjVID1mAqZyhD3p0VbJtidKAxMHUwLmEMaCAJX0UN mahowald'
+node.default['fb_ssh']['authorized_keys']['sheila']['sheila'] = 
+  'ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINy9cJcJl8oN6bRtcBc4RZq8f/T6P1AFR3YS1YRYi5YY sheila'
