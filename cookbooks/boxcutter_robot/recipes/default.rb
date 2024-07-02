c9036a5152e480dd0088b89c291226f4560d02dc
Mischa Taylor <taylor@linux.com>
2024-07-02 17:52:58 +0000

    Update server IP

--- cookbooks/boxcutter_robot/recipes/default.rb
+++ cookbooks/boxcutter_robot/recipes/default.rb
@@ -44,7 +44,7 @@ if nfs_client_hosts
   include_recipe 'boxcutter_nfs::client'
 
   node.default['fb_fstab']['mounts'][''] = {
-    'device' => '10.63.46.39:/var/nfs/general',
+    'device' => '10.63.46.196:/var/nfs/general',
     'mount_point' => '/mnt/server',
     'type' => 'nfs',
   }
