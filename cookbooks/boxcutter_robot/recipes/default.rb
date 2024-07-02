6199a61814189afb6fea74c71b1b4bd9565e3eaa
Mischa Taylor <taylor@linux.com>
2024-07-02 00:11:07 +0000

    Try nfs client on robot

--- cookbooks/boxcutter_robot/recipes/default.rb
+++ cookbooks/boxcutter_robot/recipes/default.rb
@@ -15,3 +15,37 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+nfs_client_hosts = %w(
+  nfs-client-centos-stream-9
+  nfs-client-ubuntu-2204
+).include?(node['hostname'])
+
+if nfs_client_hosts
+  node.default['fb_iptables']['filter']['INPUT']['rules']['nfs server'] = {
+    'rules' => [
+      '-p tcp --dport 2049 -j ACCEPT',
+      '-p udp --dport 2049 -j ACCEPT',
+    ],
+  }
+
+  directory '/mnt' do
+    owner node.root_user
+    group node.root_group
+    mode '0755'
+  end
+
+  directory '/mnt/server' do
+    owner 'nobody'
+    group node.ubuntu? ? 'nogroup' : 'nobody'
+    mode '0777'
+  end
+
+  include_recipe 'boxcutter_nfs::client'
+
+  node.default['fb_fstab']['mounts'][''] = {
+    'device' => '10.63.46.39:/var/nfs/general',
+    'mount_point' => '/mnt/server',
+    'type' => 'nfs',
+  }
+end
