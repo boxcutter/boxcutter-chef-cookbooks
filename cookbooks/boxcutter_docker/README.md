39ac1eea8c53f7f51479ee65f3bad11333265872
Mischa Taylor <taylor@linux.com>
2024-07-12 19:35:55 -0400

    Add chefspec to docker cookbook

--- cookbooks/boxcutter_docker/README.md
+++ cookbooks/boxcutter_docker/README.md
@@ -7,8 +7,50 @@ Configure the Docker daemon and containers.
 - node['boxcutter_docker']['enable']
 - node['boxcutter_docker']['group']
 - node['boxcutter_docker']['config']
-- node['boxcutter_docker']['containers']
+- node['boxcutter_docker']['buildkits']
 - node['boxcutter_docker']['contexts']
+- node['boxcutter_docker']['containers']
 - node['boxcutter_docker']['bind_mounts']
 - node['boxcutter_docker']['volumes']
 - node['boxcutter_docker']['devices']
+- node['boxcutter_docker']['networks']
+
+## Usage
+
+### BuildKit
+
+You can use the `contexts` attribute to define connections to other Docker
+daemons.
+
+The following example will create a new context called `docker-test` and specifies the
+host endpoint of the context to TCP socket `tcp://docker:2375`.
+
+```
+node['boxcutter_docker']['contexts']['docker-test'] = {
+  'docker' => 'host=tcp://docker:2375'
+}
+```
+
+You can use the `buildkits` attribute to manage dedicated BuildKit containers
+using Docker. These dedicated BuildKit containers are needed for multi-platform
+builds because the default [docker driver](https://docs.docker.com/build/drivers/docker/)
+does not support building multiple platforms with `docker buildx build`
+
+```
+node['boxcutter_docker']['buildkits']['x86_64_builder'] = {
+  'name' => 'x86-64-builder',
+  'use' => true,
+}
+```
+
+```
+node['boxcutter_docker']['contexts']['docker-test'] = {
+  'docker' => 'host=ssh://user@remote_host',
+  'description' => 'My Remote Docker Host'
+}
+node['boxcutter_docker']['contexts']['context1'] = {
+  'name' => 'foo',
+  'docker' => 'host=ssh://user@remote_host',
+  'description' => 'My Remote Docker Host'
+}
+```
--- cookbooks/boxcutter_docker/resources/default.rb
+++ cookbooks/boxcutter_docker/resources/default.rb
@@ -1,9 +1,26 @@
 unified_mode true
 
 action :configure do
+  # contexts
+  all_contexts = context_ls
+
+  node['boxcutter_docker']['contexts'].each do |name, data|
+    if !all_contexts[name]
+      context_create(name, data)
+    end
+  end
+
+  # buildkits
+  all_buildkits = buildx_ls
+
+  node['boxcutter_docker']['buildkits'].each do |name, data|
+    if !all_buildkits[name]
+      buildx_create(name, data)
+    end
+  end
+
+  # networks
   current_networks = network_ls
-  current_containers = container_ls
-  current_volumes = volume_ls
 
   node['boxcutter_docker']['networks'].each do |name, data|
     if !current_networks[name]
@@ -11,13 +28,18 @@ action :configure do
     end
   end
 
+  # volumes
+  current_volumes = volume_ls
+
   node['boxcutter_docker']['volumes'].each do |name, data|
     if !current_volumes[name]
       volume_create(name, data)
     end
   end
 
+  # bind_mounts
   node['boxcutter_docker']['bind_mounts'].each do |resource_name, data|
+    # log 'hi'
     name = data['path'] || resource_name
 
     directory name do
@@ -26,6 +48,9 @@ action :configure do
       owner data['owner']
     end
   end
+
+  # containers
+  current_containers = container_ls
 
   node['boxcutter_docker']['containers'].each do |name, data|
     desired_state = 'running'
@@ -42,13 +67,19 @@ action :configure do
     end
 
     with_run_context :root do
-      service "boxcutter_docker #{name}" do
+      service "boxcutter_docker container #{name}" do
         provider Chef::Provider::Service::Simple
         start_command "docker container start #{name}"
         stop_command "docker container stop #{name}"
         status_command(
-          "[ $(docker container ls --filter 'name=${name}' --quiet | wc -l) -gt 0 ]",
+          "[ $(docker container ls --quiet --filter 'name=${name}' | wc -l) -gt 0 ]",
           )
+        if data['only_if']
+          only_if { data['only_if'].call }
+        end
+        data['restart_resources']&.each do |restart_resource|
+          subscribes :restart, restart_resource
+        end
         action service_action
       end
     end
@@ -56,6 +87,64 @@ action :configure do
 end
 
 action_class do
+  # contexts
+  def context_ls
+    result = shell_out!('docker context ls --format "{{json .}}"')
+    contexts = {}
+    result.stdout.each_line do |line|
+      data = JSON.parse(line.strip)
+      contexts[data['Name']] = {
+        'docker_endpoint' => data['DockerEndpoint'],
+        'context_type' => data['ContextType'],
+        'current' => data['Current'],
+      }
+    end
+    contexts
+  end
+  def context_create_command(name, data)
+    cmd = ["docker context create #{name}"]
+    cmd << data['docker_endpoint']
+    cmd << '--use' if data.fetch('use', false)
+    cmd.join(' ')
+  end
+
+  def context_create(name, data)
+    command = context_create_command(name, data)
+    execute "docker context create #{name}" do
+      command command
+    end
+  end
+
+  # buildkits
+  def buildx_ls
+    result = shell_out!('docker buildx ls --format "{{json .}}"')
+    buildkits = {}
+    result.stdout.each_line do |line|
+      data = JSON.parse(line.strip)
+      buildkits[data['Name']] = {
+        'driver' => data['Driver'],
+        'labels' => data['Labels'],
+      }
+    end
+    buildkits
+  end
+
+  def buildx_create_command(name, data)
+    cmd = ["docker buildx create --name #{name}"]
+    cmd << '--use' if data.fetch('use', false)
+    cmd.join(' ')
+  end
+
+  def buildx_create(name, data)
+    command = buildx_create_command(name, data)
+    puts "MISCHA: buildx_create_command=#{command}"
+    Chef::Log.debug("boxcutter_docker: buildx_create_command=#{command}")
+    execute "docker buildx create #{name}" do
+      command command
+    end
+  end
+
+  # networks
   def network_ls
     result = shell_out!('docker network ls --no-trunc --format "{{json .}}"')
     networks = {}
@@ -82,6 +171,7 @@ action_class do
     end
   end
 
+  # volumes
   def volume_ls
     result = shell_out!('docker volume ls --format "{{json .}}"')
     volumes = {}
@@ -110,6 +200,7 @@ action_class do
     end
   end
 
+  # containers
   def container_ls
     result = shell_out!('docker container ls --all --no-trunc --format "{{json .}}"')
     containers = {}
@@ -157,8 +248,8 @@ action_class do
 
   def container_run(name, data)
     command = container_run_command(name, data)
-    puts "MISCHA: container_run_command=#{command}"
-    Chef::Log.debug("boxcutter_docker: container_run_command=#{command}")
+    # puts "MISCHA: container_run_command=#{command}"
+    # Chef::Log.debug("boxcutter_docker: container_run_command=#{command}")
     execute "container run #{name}" do
       command command
     end
