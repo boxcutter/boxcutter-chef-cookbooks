0e8725893fbba0d4a3b9f790f40876f02dcea25b
Mischa Taylor <57647141+taylorific@users.noreply.github.com>
2024-09-04 22:38:38 -0400

    Add keys for taylor

--- cookbooks/boxcutter_users/recipes/default.rb
+++ cookbooks/boxcutter_users/recipes/default.rb
@@ -42,13 +42,23 @@ node.default['fb_sudo']['users']['%sudo']['dont prompt for password'] = 'ALL=NOP
 node.default['fb_ssh']['enable_central_authorized_keys'] = true
 
 node.default['fb_ssh']['authorized_keys']['taylor']['mahowald'] =
-  'ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDBZjVID1mAqZyhD3p0VbJtidKAxMHUwLmEMaCAJX0UN mahowald'
+  'ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEV40AiHWQUCXY7Yh3s5Vj/ZtRc1BWex6D2+eoEnRXM7 mahowald'
+node.default['fb_ssh']['authorized_keys']['taylor']['mahowald_home'] =
+  'ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBCgywUNFPmqOmYm1kThp++UV+tFR+VX8zEbRoemD/CQ mahowald-home'
+node.default['fb_ssh']['authorized_keys']['taylor']['mahowald_boxcutter'] =
+  'ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJjcnD2uG+DPInpQcYZpmZtMevxMzVT5yKIi2+cT4rsq mahowald-boxcutter'
 node.default['fb_ssh']['authorized_keys']['taylor']['sheila'] =
   'ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINy9cJcJl8oN6bRtcBc4RZq8f/T6P1AFR3YS1YRYi5YY sheila'
 node.default['fb_ssh']['authorized_keys']['taylor']['sheila_home'] =
   'ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMQs5gtlb+OtvTvdFu8ujoI+G4ElkJudZpxDtnwCZybA sheila-home'
-node.default['fb_ssh']['authorized_keys']['taylor']['crake_chef_workstation'] =
-  'ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFgc04AMr0cqL3Il8l+jSuRtgHAjntGzDltnOCB2Uihz crakeChefWorkstation'
+node.default['fb_ssh']['authorized_keys']['taylor']['sheila_boxcutter'] =
+  'ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIER06I4w+0j+CCvu36b9aCGA1HDDx1EOEKJ3inZSGDrw shelia-boxcutter'
+node.default['fb_ssh']['authorized_keys']['taylor']['joan'] =
+  'ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHGTw44QBehDXY6ebitrYydyAAhDFLBSkQ59RovcVsvX joan'
+node.default['fb_ssh']['authorized_keys']['taylor']['joan_home'] =
+  'ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICV7K2rMcsUmT1v1VChPu552ux/GXe/CWWmAkj7ryo9N joan-home'
+node.default['fb_ssh']['authorized_keys']['taylor']['joan_boxcutter'] =
+  'ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPxZsm6W83hWKZ+T0RLGtx6E8scnjq2o3GMjkXx+P1Bi joan-boxcutter'
 node.default['fb_ssh']['authorized_keys']['sheila']['mahowald'] =
   'ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMC7b+X2a0mRV8A7W5zolbrkALqFizKtuhmM+xZWKohl sheila@mahowald'
 node.default['fb_ssh']['authorized_keys']['david']['primary'] =
