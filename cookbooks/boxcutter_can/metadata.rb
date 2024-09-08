a98a9595b07fc856f444ceabbfbe7229628012c3
Mischa Taylor <taylor@linux.com>
2024-09-08 09:33:56 -0400

    Fix lint issues

--- cookbooks/boxcutter_can/metadata.rb
+++ cookbooks/boxcutter_can/metadata.rb
@@ -5,5 +5,4 @@ license 'Apache-2.0'
 description 'Installs/Configures boxcutter_can'
 source_url 'https://github.com/boxcutter/boxcutter-chef-cookbooks/'
 version '0.1.0'
-depends 'fb_modprobe:wq
-'
+depends 'fb_modprobe'
