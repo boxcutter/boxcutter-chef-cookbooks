f434108d9812afa54ff7b96c3d5a68410c1f33e2
Mischa Taylor <taylor@linux.com>
2024-09-22 08:35:01 -0400

    Fix lint issues

--- cookbooks/boxcutter_builder/recipes/default.rb
+++ cookbooks/boxcutter_builder/recipes/default.rb
@@ -161,7 +161,7 @@ if arm64_self_hosted_runner_list.include?(node['hostname'])
         'name' => 'mybuilder',
         'driver' => 'docker',
         'use' => true,
-      }
-    }
+      },
+    },
   }
 end
