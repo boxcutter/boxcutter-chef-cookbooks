58e137012ac4fb325c6a0ebe5434a15bffdd53b0
Mischa Taylor <taylor@linux.com>
2025-04-10 12:23:52 -0400

    Use command-delimited syntax for named_run_list

--- cookbooks/boxcutter_python/Policyfile.rb
+++ cookbooks/boxcutter_python/Policyfile.rb
@@ -12,8 +12,8 @@ default_source :chef_repo, '../'
 
 # run_list: chef-client will run these recipes in the order specified.
 run_list 'boxcutter_ohai', 'boxcutter_init', 'boxcutter_python_test::default'
-named_run_list :boxcutter_python_test_pyenv, %w{boxcutter_ohai boxcutter_init boxcutter_python_test::pyenv}
-named_run_list :boxcutter_python_test_system, %w{boxcutter_ohai boxcutter_init boxcutter_python_test::system}
+named_run_list 'boxcutter_python_test_pyenv', 'boxcutter_ohai', 'boxcutter_init', 'boxcutter_python_test::pyenv'
+named_run_list 'boxcutter_python_test_system', 'boxcutter_ohai', 'boxcutter_init', 'boxcutter_python_test::system'
 
 # Specify a custom source for a single cookbook:
 cookbook 'boxcutter_python', path: '.'
